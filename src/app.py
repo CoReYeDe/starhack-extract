a657f1c67733884993f905d5f9ab84934bb7f97d
Sandro W <git@coreye.de>
2022-06-21 09:34:29 +0200

    Init Test-Dockerfile

---
+++ src/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Hello Waste Watchers")
+st.header("Init-WebApp")
+initial = 'Test'
+
+# perform cashflow projections for the next 5 years
+st.write(f'This is a {initial}')
